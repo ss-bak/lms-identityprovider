b26cc1e8df0e56c2453e04b8a01f0754bc89de6d
Krissada Jindanupajit <krissada.jindanupajit@smoothstack.com>
2020-02-28 15:01:09 -0500

    *.properties separation

--- src/main/java/com/smoothstack/lms/auth/AuthApplication.java
+++ src/main/java/com/smoothstack/lms/auth/AuthApplication.java
@@ -2,8 +2,11 @@ package com.smoothstack.lms.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:application.properties")
+@PropertySource("classpath:database.properties")
 public class AuthApplication {
 
 	public static void main(String[] args) {
